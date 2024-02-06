0fde77d88b5001c4b4610b6410fb74d683b1c67c
Gregory Hugo <70943340+greg-hugo@users.noreply.github.com>
2024-02-06 12:47:10 +0700

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -397,7 +397,6 @@ class MobileOne(nn.Module):
         self, in_channels, out_channels, n, k, stride=1, dilation=1, padding_mode="zeros", deploy=False, use_se=False
     ):
         super().__init__()
-        print("MobileOne Stride: ", stride)
         self.m = nn.Sequential(*[MobileOneBlock(in_channels, out_channels, k, stride, deploy) for _ in range(n)])
 
     def forward(self, x):
