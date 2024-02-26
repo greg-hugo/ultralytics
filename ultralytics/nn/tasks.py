4a51cfe0d5f5b4e474743e72207f83ba7f518e8f
Gregory Hugo <70943340+greg-hugo@users.noreply.github.com>
2024-02-26 16:20:21 +0700

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -820,7 +820,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             args.insert(1, [ch[x] for x in f])
         else:
             c2 = ch[f]
-
+        print(args)
         m_ = nn.Sequential(*(m(*args) for _ in range(n))) if n > 1 else m(*args)  # module
         t = str(m)[8:-2].replace("__main__.", "")  # module type
         m.np = sum(x.numel() for x in m_.parameters())  # number params
