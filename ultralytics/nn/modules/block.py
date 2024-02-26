8b04d39bcae47f3c977393e37b4368274cdedd7e
Gregory Hugo <70943340+greg-hugo@users.noreply.github.com>
2024-02-26 10:07:09 +0700

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, MobileOneBlock, gOctCBR
+from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, MobileOneBlock, gOctaveCBR
 from .transformer import TransformerBlock
 
 __all__ = (
