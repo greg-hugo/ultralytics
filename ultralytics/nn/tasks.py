99e2c56e2a5d0943ed82e10a0e2d5bbbdd8764be
greg-hugo <hugo.gregory5@gmail.com>
2024-03-29 23:26:19 +0700

    a

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -248,8 +248,8 @@ class BaseModel(nn.Module):
         """
         model = weights["model"] if isinstance(weights, dict) else weights  # torchvision models are not dicts
         csd = model.float().state_dict()  # checkpoint state_dict as FP32
+        print(csd.keys())
         csd = intersect_dicts(csd, self.state_dict())  # intersect
-        print(csd)
         self.load_state_dict(csd, strict=False)  # load
         if verbose:
             LOGGER.info(f"Transferred {len(csd)}/{len(self.model.state_dict())} items from pretrained weights")
